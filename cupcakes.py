67286fbf9f7791cd787ffc8932ff9cc69b1d91cc
Victor Pecine <73555379+victorpecine@users.noreply.github.com>
2022-10-03 19:56:44 -0300

    analise a aumento dos graus de liberdade

--- cupcakes.py
+++ cupcakes.py
@@ -1,4 +1,5 @@
 from statistics import mode
+import numpy as np
 import pandas as pd
 import pyDOE2 as doe
 import statsmodels.api as sm
@@ -44,3 +45,49 @@ modelo_ajustado = modelo.fit()
 # Skew:                           0.000   Prob(JB):                        0.920
 # Kurtosis:                       2.000   Cond. No.                         1.00
 # ==============================================================================
+
+# Graus de liberdade = 0
+
+# Aumento dos graus de liberdade (1 kg de farinha, 0,3 kg de chocolate)
+centro_area_experimentacao = np.array([[0, 0, 29],
+                                       [0, 0, 30],
+                                       [0, 0, 29],
+                                       [0, 0, 30]])
+
+df_centro = pd.DataFrame(centro_area_experimentacao, columns=['farinha', 'chocolate', 'quantidade'], index=[4, 5, 6, 7])
+
+
+# Mescla dos dataframes
+df_ensaios = pd.concat([df_ensaios, df_centro])
+
+
+# Novo modelo para teste de significância estatística
+modelo = smf.ols(data=df_ensaios, formula='quantidade ~ farinha + chocolate + farinha:chocolate')
+
+modelo_ajustado = modelo.fit()
+#                             OLS Regression Results
+# ==============================================================================
+# Dep. Variable:             quantidade   R-squared:                       0.971
+# Model:                            OLS   Adj. R-squared:                  0.950
+# Method:                 Least Squares   F-statistic:                     45.21
+# Date:                Mon, 03 Oct 2022   Prob (F-statistic):            0.00152
+# Time:                        19:47:35   Log-Likelihood:                -14.155
+# No. Observations:                   8   AIC:                             36.31
+# Df Residuals:                       4   BIC:                             36.63
+# Df Model:                           3
+# Covariance Type:            nonrobust
+# =====================================================================================
+#                         coef    std err          t      P>|t|      [0.025      0.975]
+# -------------------------------------------------------------------------------------
+# Intercept            30.8750      0.710     43.494      0.000      28.904      32.846
+# farinha              10.7500      1.004     10.708      0.000       7.963      13.537
+# chocolate             4.2500      1.004      4.233      0.013       1.463       7.037
+# farinha:chocolate     1.7500      1.004      1.743      0.156      -1.037       4.537
+# ==============================================================================
+# Omnibus:                        4.655   Durbin-Watson:                   0.841
+# Prob(Omnibus):                  0.098   Jarque-Bera (JB):                1.080
+# Skew:                          -0.180   Prob(JB):                        0.583
+# Kurtosis:                       1.237   Cond. No.                         1.41
+# ==============================================================================
+
+# Graus de liberdade = 4
